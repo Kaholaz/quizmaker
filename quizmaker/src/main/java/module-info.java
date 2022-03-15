ec53fbdd87935491b9d724dee8841abab003acad
Arunan <arunan2212@gmail.com>
2022-03-15 13:13:41 +0100

    FIX MODULE INFO !!!!!!!!!!!!!!!!!!

--- quizmaker/src/main/java/module-info.java
+++ quizmaker/src/main/java/module-info.java
@@ -1,6 +1,6 @@
-module org.ntnu.k2.g2.quizmaker {
+open module org.ntnu.k2.g2.quizmaker {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    exports org.ntnu.k2.g2.quizmaker.GUI;
+    exports org.ntnu.k2.g2.quizmaker;
 }
