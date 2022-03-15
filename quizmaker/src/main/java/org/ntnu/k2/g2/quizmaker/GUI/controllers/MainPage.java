e277e3e2548169e9dd92eefa1163cab211b19a15
Eric B <ericbieszczad@gmail.com>
2022-03-15 16:17:34 +0100

    Fixes.

--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/MainPage.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/MainPage.java
@@ -1,4 +1,29 @@
 package org.ntnu.k2.g2.quizmaker.GUI.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainPage {
+
+    @FXML
+    public Button btnNewQuiz, btnListQuizzes;
+
+    public void onListQuizzesBtnClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/listActiveQuizzesPage.fxml"));
+        Stage stage = (Stage) btnNewQuiz.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+
+    public void onCreateNewQuizBtnClicked() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/listActiveQuizzesPage.fxml"));
+        Stage stage = (Stage) btnNewQuiz.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
 }
