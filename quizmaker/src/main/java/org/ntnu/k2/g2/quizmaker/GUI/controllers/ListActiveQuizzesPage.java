97bce1b327fdc4fe06aab3278d9620d6c5903c60
Arunan <arunan2212@gmail.com>
2022-03-15 16:55:49 +0100

    Add Browse functionality

--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/ListActiveQuizzesPage.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/ListActiveQuizzesPage.java
@@ -2,8 +2,12 @@ package org.ntnu.k2.g2.quizmaker.GUI.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import org.ntnu.k2.g2.quizmaker.Data.Question;
+import org.ntnu.k2.g2.quizmaker.Data.Quiz;
+import org.ntnu.k2.g2.quizmaker.Data.QuizRegister;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,25 +20,21 @@ public class ListActiveQuizzesPage implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            this.updateQuizzes();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.updateQuizzes();
     }
 
-    private void updateQuizzes() throws IOException {
-        ArrayList<String> strings = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
-            strings.add("joie");
-            strings.add("hei");
-        }
+    private void updateQuizzes() {
+        QuizRegister quizRegister = new QuizRegister();
+        quizRegister.populateDatabase(10, 5, 5);
+        ArrayList<Quiz> quizzes = quizRegister.getQuizList();
 
-        int index = 0;
-        for (String string : strings) {
+        int index = 1;
+        for (Quiz quiz : quizzes) {
             Text text = new Text();
-            text.setText(string);
+            Button button = new Button("Admin");
+            text.setText(quiz.getName());
             quizzesContainer.add(text, 0, index);
+            quizzesContainer.add(button, 1, index);
             index++;
         }
     }
