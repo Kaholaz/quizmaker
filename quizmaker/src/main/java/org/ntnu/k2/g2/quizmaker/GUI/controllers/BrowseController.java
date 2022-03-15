e0b87e6cfef758b45c592bc588515254057add85
Arunan <arunan2212@gmail.com>
2022-03-15 13:39:53 +0100

    Change controller names

--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/BrowseController.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/BrowseController.java
@@ -1,6 +1,6 @@
 package org.ntnu.k2.g2.quizmaker.GUI.controllers;
 
-public class browseController {
+public class BrowseController {
 
 
 
--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/MainPageController.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/MainPageController.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class mainPageController {
+public class MainPageController {
 
     @FXML
     public Button newQuiz, browse;
