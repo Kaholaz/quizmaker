34bafc971b845256df11abf76769dc58f42dd165
Arunan <arunan2212@gmail.com>
2022-03-15 12:33:47 +0100

    Try sceneswitching

--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/mainPageController.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/mainPageController.java
@@ -1,12 +1,30 @@
 package org.ntnu.k2.g2.quizmaker.GUI;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class mainPageController {
+
+    @FXML
+    public Button newQuiz;
 
     public void browse(ActionEvent actionEvent) {
     }
 
-    public void newQuiz(ActionEvent actionEvent) {
+    public void newQuiz() throws IOException {
+        URL url = new File("src/main/resources/GUI/newQuiz.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Stage stage = (Stage) newQuiz.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        
     }
 }
