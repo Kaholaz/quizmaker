ea1fade11f73344aee01a87379a8b05d7681d6a0
Arunan <arunan2212@gmail.com>
2022-03-16 12:17:25 +0100

    Add rest of pages

--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/ListActiveQuizzesPage.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/ListActiveQuizzesPage.java
@@ -1,31 +1,64 @@
+
+/**
+ * Sample Skeleton for 'listActiveQuizzesPage.fxml' Controller Class
+ */
+
 package org.ntnu.k2.g2.quizmaker.GUI.controllers;
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
-import org.ntnu.k2.g2.quizmaker.Data.Question;
-import org.ntnu.k2.g2.quizmaker.Data.Quiz;
-import org.ntnu.k2.g2.quizmaker.Data.QuizRegister;
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import org.ntnu.k2.g2.quizmaker.Data.Quiz;
+import org.ntnu.k2.g2.quizmaker.Data.QuizRegister;
 
-public class ListActiveQuizzesPage implements Initializable {
+public class ListActiveQuizzesPage {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="archive"
+    private Button archive; // Value injected by FXMLLoader
+
+    @FXML // fx:id="back"
+    private Button back; // Value injected by FXMLLoader
+
+    @FXML // fx:id="quizzesContainer"
+    private GridPane quizzesContainer; // Value injected by FXMLLoader
+
+    @FXML // fx:id="scrollPane"
+    private ScrollPane scrollPane; // Value injected by FXMLLoader
+
     @FXML
-    public GridPane quizzesContainer;
+    void onArchive(ActionEvent event) {
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    @FXML
+    void onBack(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert archive != null : "fx:id=\"archive\" was not injected: check your FXML file 'listActiveQuizzesPage.fxml'.";
+        assert back != null : "fx:id=\"back\" was not injected: check your FXML file 'listActiveQuizzesPage.fxml'.";
+        assert quizzesContainer != null : "fx:id=\"quizzesContainer\" was not injected: check your FXML file 'listActiveQuizzesPage.fxml'.";
+        assert scrollPane != null : "fx:id=\"scrollPane\" was not injected: check your FXML file 'listActiveQuizzesPage.fxml'.";
         this.updateQuizzes();
     }
 
+
     private void updateQuizzes() {
         QuizRegister quizRegister = new QuizRegister();
-        quizRegister.populateDatabase(10, 5, 5);
         ArrayList<Quiz> quizzes = quizRegister.getQuizList();
 
         int index = 1;
@@ -38,4 +71,5 @@ public class ListActiveQuizzesPage implements Initializable {
             index++;
         }
     }
+
 }
