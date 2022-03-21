f9e6de558cf0ef0e4f662c60579612e20021014c
Isak Jonsson <isaksj@stud.ntnu.no>
2022-03-21 10:23:31 +0100

    Initial functionality for pdf exports implemented

--- quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/QuizSystem.java
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/QuizSystem.java
@@ -1,4 +1,41 @@
 package org.ntnu.k2.g2.quizmaker;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.FileNotFoundException;
+
 public class QuizSystem {
+
+    public void exportPdf() {
+        // Creating a PdfWriter
+        String dest = "C:/Desktop/addingParagraph.pdf";
+        PdfWriter writer = null;
+        try {
+            writer = new PdfWriter(dest);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Creating a PdfDocument
+        PdfDocument pdf = new PdfDocument(writer);
+
+        // Creating a Document
+        Document document = new Document(pdf);
+        String para1 = "Her kan man skrive et spørsmål";
+        String para2 = "Her kan man skrive et annet spørsmål";
+
+        // Creating Paragraphs
+        Paragraph paragraph1 = new Paragraph(para1);
+        Paragraph paragraph2 = new Paragraph(para2);
+
+        // Adding paragraphs to document
+        document.add(paragraph1);
+        document.add(paragraph2);
+
+        // Closing the document
+        document.close();
+    }
 }
