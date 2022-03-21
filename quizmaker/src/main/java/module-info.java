019caa139b266dd9e89ec449534d107fdb88bb09
Isak Jonsson <isaksj@stud.ntnu.no>
2022-03-21 10:34:10 +0100

    Initial functionality for pdf exports implemented

--- quizmaker/src/main/java/module-info.java
+++ quizmaker/src/main/java/module-info.java
@@ -2,5 +2,7 @@ open module org.ntnu.k2.g2.quizmaker {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires kernel;
+    requires layout;
     exports org.ntnu.k2.g2.quizmaker;
 }
