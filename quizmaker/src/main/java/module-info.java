dea07c51604d1342a9952a6d331b4c1310325dfc
ingrid <ingridmegge@gmail.com>
2022-03-23 11:31:57 +0100

    Added methods to create and modify Google Sheets.

--- quizmaker/src/main/java/module-info.java
+++ quizmaker/src/main/java/module-info.java
@@ -2,5 +2,13 @@ open module org.ntnu.k2.g2.quizmaker {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires google.http.client;
+    requires google.http.client.gson;
+    requires google.api.services.sheets.v4.rev493;
+    requires google.http.client.jackson2;
+    requires google.api.client;
+    requires google.oauth.client.jetty;
+    requires google.oauth.client.java6;
+    requires google.oauth.client;
     exports org.ntnu.k2.g2.quizmaker;
 }
---
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/UserData/QuizResultManager.java
@@ -0,0 +1,60 @@
+package org.ntnu.k2.g2.quizmaker.UserData;
+import org.ntnu.k2.g2.quizmaker.Data.Quiz;
+import org.ntnu.k2.g2.quizmaker.Data.QuizRegister;
+import org.ntnu.k2.g2.quizmaker.Data.Team;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+public class QuizResultManager {
+
+    /**
+     *
+     * @param quizId
+     * @param quizName
+     * @throws GeneralSecurityException
+     * @throws IOException
+     */
+    public String createResultSheet(int quizId, String quizName) throws GeneralSecurityException, IOException {
+        QuizRegister quizRegister = new QuizRegister();
+        Quiz quiz = quizRegister.getQuiz(quizId);
+
+        ResultSheet resultSheet = new ResultSheet();
+
+        String sheetId = resultSheet.createSheet(quizName);
+        quiz.setUrl("https://docs.google.com/spreadsheets/d/" + sheetId);
+
+        quizRegister.saveQuiz(quiz);
+
+        return sheetId;
+    }
+
+    /**
+     * Adds the results from the quiz result-sheet to the database
+     * @param quizResult List<List<Object>> list containing the results from the quiz fetched from result sheet
+     * @param quizId quiz id in the database
+     */
+    public void registerResults(int quizId, List<List<Object>> quizResult){
+        QuizRegister quizRegister = new QuizRegister();
+        Quiz quiz = quizRegister.getQuiz(quizId);
+
+        quiz.getTeams().keySet().stream().forEach(teamId-> quizRegister.removeTeam(quiz, teamId));
+
+        for (List row : quizResult) {
+            String teamName = row.get(0).toString();
+            int score = (Integer)row.get(1);
+
+            Team team = quizRegister.newTeam(quiz);
+            team.setTeamName(teamName);
+            team.setScore(score);
+        }
+        quizRegister.saveQuiz(quiz);
+    }
+
+
+
+
+
+
+}
