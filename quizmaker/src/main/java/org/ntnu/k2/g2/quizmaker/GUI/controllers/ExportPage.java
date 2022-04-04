a4f58a434647cecffa2b07039681ddb2c5b8f68f
Isak Jonsson <isaksj@stud.ntnu.no>
2022-04-04 21:07:18 +0200

    Fixes of local file issues

---
+++ quizmaker/src/main/java/org/ntnu/k2/g2/quizmaker/GUI/controllers/ExportPage.java
@@ -0,0 +1,77 @@
+package org.ntnu.k2.g2.quizmaker.GUI.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import org.ntnu.k2.g2.quizmaker.GUI.QuizHandlerSingelton;
+import org.ntnu.k2.g2.quizmaker.PdfExport.PdfManager;
+import org.ntnu.k2.g2.quizmaker.data.QuizModel;
+
+import java.io.File;
+
+/**
+ * Controller for the exportQAPage. It handles what types of pdfs should be exported.
+ */
+
+public class ExportPage {
+    @FXML // fx:id="export"
+    private Button export; // Value injected by FXMLLoader
+
+    @FXML
+    private Text exportMsg;
+
+    @FXML
+    private CheckBox c1, c2, c3, c4, c5, c6;
+
+    private final QuizModel quiz = QuizHandlerSingelton.getQuiz();
+
+    /**
+     * Checks the checkboxes. If they are selected it will import the selected option.
+     */
+
+    @FXML
+    private void onExport() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        Stage stage = (Stage) export.getScene().getWindow();
+        File file = directoryChooser.showDialog(stage);
+        String msg = "Export successful";
+        int i = 0;
+
+        try {
+            if (c1.isSelected()) {
+                PdfManager.exportAnswersheetWithoutQuestions(quiz,file.toString());
+                i++;
+            }
+            if (c2.isSelected()) {
+                PdfManager.exportAnswersheetWithQuestions(quiz,file.toString());
+                i++;
+            }
+            if (c3.isSelected()) {
+                PdfManager.exportAnswersheetWithoutQuestions(quiz,file.toString());
+                i++;
+            }
+            if (c4.isSelected()) {
+                PdfManager.exportAnswersheetWithoutQuestions(quiz,file.toString());
+                i++;
+            }
+            if (c5.isSelected()) {
+                PdfManager.exportAnswersWithQuestions(quiz,file.toString());
+                i++;
+            }
+            if (c6.isSelected()) {
+                PdfManager.exportAnswersWithoutQuestions(quiz,file.toString());
+                i++;
+            }
+        } catch (Exception e) {
+            msg = "Export Unsuccessful" + e.getMessage();
+        }
+        if (i > 0) {
+            exportMsg.setText(msg);
+        } else {
+            exportMsg.setText("Nothing was exported.");
+        }
+    }
+}
