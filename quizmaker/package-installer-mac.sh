d146e159d67f1b25e903722bcdae610b895a78b3
Sebastian Bugge <vkbugge@hotamail.com>
2022-04-27 09:57:06 +0200

    fixup! Added installer packageing script for mac

--- quizmaker/package-installer-mac.sh
+++ quizmaker/package-installer-mac.sh
@@ -10,8 +10,9 @@ if [ -e src/main/resources/userdata/google-credentials.json ]
 then
     if ! mvn clean package shade:shade -DskipTests; then error; fi
     cd target
+    echo "Creating installer... this may take a while."
     if ! jpackage --input . --name Quizmaker --main-jar quizmaker-1.0-beta.jar --main-class org.ntnu.k2.g2.quizmaker.Main --icon classes/gui/media/quiz-logo-transparent.icns --vendor k2g2 --copyright k2g2 --type dmg --app-version 1.0.0; then error; fi
-    echo "Installer sucsessfully created!"
+    echo "Installer sucsessfully created! (check the 'target' folder)"
     read  -n 1 -p "Press any key to exit..." _
     exit 0
 else
