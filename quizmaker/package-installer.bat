f8c560f208e9fa97fa4a57b0be2a6245966e71f0
Sebastian Bugge <vkbugge@hotmail.com>
2022-04-29 00:18:39 +0200

    Changed version to 1.0.0

--- quizmaker/package-installer.bat
+++ quizmaker/package-installer.bat
@@ -3,7 +3,7 @@ if exist src\main\resources\userdata\google-credentials.json (
     mvn clean package shade:shade -DskipTests || goto ERROR
     cd target
     echo Creating installer... this may take a while!
-    jpackage --input . --name Quizmaker --main-jar .\quizmaker-1.0-beta.jar --main-class org.ntnu.k2.g2.quizmaker.Main --icon .\classes\gui\media\quiz-logo-transparent.ico  --type exe --win-dir-chooser --vendor k2g2 --copyright k2g2 --win-menu --win-shortcut --win-upgrade-uuid 863281a1-ac73-44de-a77f-d0e6dd1fcd77 --app-version 1.0.0 || goto ERROR
+    jpackage --input . --name Quizmaker --main-jar .\quizmaker-1.0.0.jar --main-class org.ntnu.k2.g2.quizmaker.Main --icon .\classes\gui\media\quiz-logo-transparent.ico  --type exe --win-dir-chooser --vendor k2g2 --copyright k2g2 --win-menu --win-shortcut --win-upgrade-uuid 863281a1-ac73-44de-a77f-d0e6dd1fcd77 --app-version 1.0.0 || goto ERROR
     echo Installer successfully created! Check the 'target' folder...
     echo Press any key to exit...
     pause >nul
